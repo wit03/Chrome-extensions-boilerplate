660d0c6b9379c66bb8d0dceba6dc4c598229c2bc
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2019-04-16 22:20:18 +0700

    Init boilerplate

---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          //Below this line (comment) is a solution to fix url on extensions
+        /*pageUrl: {
+            hostEquals: 'facebook.com'
+            urlContains: 'facebook'
+        }*/
+        })
+    ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+
+})
+
+
