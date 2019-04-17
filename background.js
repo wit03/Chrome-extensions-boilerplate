385f2f6f6d87c833fcc3130c93d53877d6a8476d
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2019-04-17 14:14:29 +0700

    finished up boilerplate
    - now it can inject script to website

--- background.js
+++ background.js
@@ -4,16 +4,23 @@ chrome.runtime.onInstalled.addListener(() => {
       conditions: [
         new chrome.declarativeContent.PageStateMatcher({
           //Below this line (comment) is a solution to fix url on extensions
-        /*pageUrl: {
-            hostEquals: 'facebook.com'
-            urlContains: 'facebook'
-        }*/
+          /*pageUrl: {
+              hostEquals: 'facebook.com'
+              urlContains: 'facebook'
+          }*/
         })
-    ],
+      ],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
 
 })
+chrome.browserAction.onClicked.addListener(function (tab) {
+	// for the current tab, inject the "inject.js" file & execute it
+	chrome.tabs.executeScript(tab.id, {
+		file: 'inject.js'
+	});
+});
+
 
 
