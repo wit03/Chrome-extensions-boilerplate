de0df1d817d530a3727980aec0b81ee2c04fe9ea
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2019-04-19 17:24:52 +0700

    Finalize

--- inject.js
+++ inject.js
@@ -1,16 +1,61 @@
-(function() {
+(function () {
 
-    //Below this line is the injected script
-    //you can edit easily as an usual javascript on website
+	//Below this line is the injected script
+	//you can edit easily as an usual javascript on website
 
 	// just place a div at top right
-	var div = document.createElement('div');
-	div.style.position = 'fixed';
-	div.style.top = 0;
-	div.style.right = 0;
-	div.textContent = 'Injected!';
-	document.body.appendChild(div);
+	//var data = []
 
 	alert('inserted self... giggity');
+	var cSite = window.location.href;
+	var img = document.getElementsByTagName("img, a, i");
+	var data = []
 
+	function checkURL(currentSite) {
+		var hostName = window.location.hostname.split('.');
+		currentSite = currentSite.split('.');
+
+		for (var x in currentSite) {
+			if (currentSite[x] == "") {
+				currentSite.splice(x, 1);
+			}
+		}
+		var j = hostName.length - currentSite.length
+		for (var i in currentSite) {
+			if (!hostName[j] || hostName[j].toLowerCase() != currentSite[i].toLowerCase()) {
+				return false;
+			}
+			j++;
+		}
+		return true;
+	}
+
+	if (checkURL("facebook.com") === true) {
+		alert("Yay it's facebook")
+		for (var i = 0; i < img.length; i++) {
+			data.push(img[i].src);
+		}
+		console.log('Facebook:' + data[1])
+	}
+
+	if (checkURL("instagram.com") === true) {
+		alert(" Yay it's instagram")
+		for (var i = 0; i < img.length; i++) {
+			data.push(img[i].src);
+		}
+		console.log('Instagram:' + data[1])
+	}
+	//not tested yet
+	if (checkURL("twitter.com") === true) {
+		alert("Yay it's twitter")
+		for (var i = 0; i < img.length; i++) {
+			data.push(img[i].src);
+		}
+		console.log('twitter:' + data[1])
+	}
+
+
+
+	//test script
+	//document.querySelectorAll("img")[1].getAttribute("src")
 })();
